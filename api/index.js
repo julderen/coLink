a1af1c0b899d134efaeaa5176b8c32b6babcf79e
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-08-01 16:36:01 +0300

    try fix bug with dbconfig

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const App_1 = require("./app/App");
 const configs_1 = require("../configs");
-const server = new App_1.default(process.env.TYPE, configs_1.default[process.env.TYPE]);
+const server = new App_1.default(process.env.TYPE, configs_1.default[process.env.TYPE] || configs_1.default.local);
 server.start();
--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,6 @@ import 'reflect-metadata';
 import App from './app/App';
 import configs from '../configs';
 
-const server = new App(process.env.TYPE, configs[process.env.TYPE]);
+const server = new App(process.env.TYPE, configs[process.env.TYPE] || configs.local);
 
 server.start();
