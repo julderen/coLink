5f9e01c5fd3755bb690d0d09fa2ef4650a32e348
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-09-17 15:02:49 +0300

    build

---
+++ templates/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const createUser_ejs_1 = require("./createUser.ejs");
+exports.default = {
+    createUser: createUser_ejs_1.default
+};
